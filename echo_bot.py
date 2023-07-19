5a536895d685b77599723365102fa8b36b9c8a34
Vasiliy Malashenko <valavas@mail.ru>
2023-07-19 13:49:50 +0300

    Написан новых Эхо-Бот

--- echo_bot.py
+++ echo_bot.py
@@ -1,14 +1,29 @@
-import requests
-import time
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 
-API_URL: str = 'https://api.telegram.org/bot'
-BOT_TOKEN: str = '5920847035:AAF1NcN2CSyfR_9X9UbcRZDJLnDNn7YCZ8g'
-TEXT: str = 'УРААААААА! ПРИВЕТТТ!!!!!'
-MAX_COUNTER: int = 100
+BOT_TOKEN: str = 'BOT TOKEN HERE'
 
-offset: int = -2
-counter: int = 0
-chat_id: int
-while counter < MAX_COUNTER:
-    print('попытка = ', counter)      # Номер попытки, чтобы видеть, что код живёт
-    updates = request.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+# Создаём объекты бота и деспетчера
+bot: Bot = Bot(token=BOT_TOKEN)
+dp: Dispatcher = Dispatcher()
+
+# Хэндлер для команды "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message) -> None:
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь!')
+
+# Хэндлер для команды "help"
+@dp.message(Command(commands=["help"]))
+async def process_help_command(message: Message) -> None:
+    await message.answer('Напиши мне что-нибуть, и я пришлю'
+                         'тебе в ответ твоё же сообщение!')
+
+# Хэндлер для любых текстовых сообщений,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message) -> None:
+    await message.reply(text=message.text)
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
